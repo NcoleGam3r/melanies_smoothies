1c8335ea635190a8fbf624f96312cba80c6747a3
Nicole Frapolly (nfrapolly) <nfrapolly@enova.com>
2025-11-10 19:56:05 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,10 +39,10 @@ def get_dropdown_options(table_name, column_name):
     query = f"SELECT DISTINCT {column_name} FROM {table_name}"
     # Use session.sql to run the query and convert to a Pandas DataFrame
     my_dataframe = cnx.query(query) 
-    df = my_dataframe.to_pandas()
+    # df = my_dataframe.to_pandas()
     
     # Extract the column values into a list
-    options_list = df[column_name].tolist()
+    options_list = my_dataframe[column_name].tolist()
     return options_list
     
    # Define your table and column names
