a1fc24c1f6a1da2cfcf5d42084d7c68d0954e192
Nicole Frapolly (nfrapolly) <nfrapolly@enova.com>
2025-11-10 21:06:38 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,3 +66,6 @@ if ingredients_list:
                 st.success("""Your Smoothie is ordered, '""" + name_on_order +"""'!""", icon="✅")
         except Exception as e:
             st.error(f"Error inserting data: {e}")
+
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/all")
+st.text(smoothiefroot_response)
