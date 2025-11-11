4de17c2bdf1e8c8c98f376605201031fb049b9ff
Nicole Frapolly (nfrapolly) <nfrapolly@enova.com>
2025-11-10 20:14:08 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,10 +61,10 @@ if ingredients_list:
             cursor = raw_conn.cursor()
             cursor.execute(my_insert_stmt)
             raw_conn.commit()
-            cursor.close()
-            raw_conn.close()
+            st.success("""Your Smoothie is ordered, '""" + name_on_order +"""'!""", icon="✅")
         except Exception as e:
             st.error(f"Error inserting data: {e}")
-
+        finally:
+            cursor.close()
+            raw_conn.close()
         
-        st.success("""Your Smoothie is ordered, '""" + name_on_order +"""'!""", icon="✅")
